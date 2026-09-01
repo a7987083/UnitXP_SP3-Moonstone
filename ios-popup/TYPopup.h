47cf0c7d68fc1f83d3e9ca4b5f52cf11bbe1b067
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-01 13:51:29 +0800

    add iOS popup module header

---
+++ ios-popup/TYPopup.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void TY_ShowPopup(const char *title, const char *message);
+void TY_ShowConfirm(const char *title, const char *message);
+
+#ifdef __cplusplus
+}
+#endif
