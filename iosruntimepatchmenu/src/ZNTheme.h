688b5cc348e2dec27288b248ed600c0f02b35793
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-06 06:12:09 +0800

    feat: expand runtime menu to 20 theme modes

--- iosruntimepatchmenu/src/ZNTheme.h
+++ iosruntimepatchmenu/src/ZNTheme.h
@@ -5,20 +5,58 @@
 
 typedef NS_ENUM(NSInteger, ZNThemeMode) {
     ZNThemeModeSystem = 0,
-    ZNThemeModeObsidian = 1,
-    ZNThemeModeNeon = 2,
-    ZNThemeModeGlacier = 3,
-    ZNThemeModeAmber = 4,
-    ZNThemeModeMatcha = 5,
+    ZNThemeModeObsidian,
+    ZNThemeModeNeon,
+    ZNThemeModeGlacier,
+    ZNThemeModeAmber,
+    ZNThemeModeMatcha,
+    ZNThemeModeGlass,
+    ZNThemeModeTerminal,
+    ZNThemeModeParchment,
+    ZNThemeModeWashiWood,
+    ZNThemeModeMedical,
+    ZNThemeModeSpaceBridge,
+    ZNThemeModeSteamWorkshop,
+    ZNThemeModeGothicArcane,
+    ZNThemeModeCandyPop,
+    ZNThemeModeInkWash,
+    ZNThemeModePolarArmory,
+    ZNThemeModeLavaForge,
+    ZNThemeModeDeepSeaSonar,
+    ZNThemeModeRetroArcade,
+    ZNThemeModeCount
+};
+
+typedef NS_ENUM(NSInteger, ZNThemeDecorationStyle) {
+    ZNThemeDecorationMinimal = 0,
+    ZNThemeDecorationObsidian,
+    ZNThemeDecorationNeonCircuit,
+    ZNThemeDecorationBlueprint,
+    ZNThemeDecorationMechanical,
+    ZNThemeDecorationMatcha,
+    ZNThemeDecorationGlass,
+    ZNThemeDecorationTerminal,
+    ZNThemeDecorationParchment,
+    ZNThemeDecorationWood,
+    ZNThemeDecorationMedical,
+    ZNThemeDecorationSpace,
+    ZNThemeDecorationSteam,
+    ZNThemeDecorationGothic,
+    ZNThemeDecorationCandy,
+    ZNThemeDecorationInk,
+    ZNThemeDecorationPolar,
+    ZNThemeDecorationLava,
+    ZNThemeDecorationSonar,
+    ZNThemeDecorationArcade
 };
 
 @interface ZNTheme : NSObject
-
 @property(nonatomic, assign, readonly) ZNThemeMode mode;
 @property(nonatomic, copy, readonly) NSString *name;
 @property(nonatomic, assign, readonly) BOOL lightAppearance;
 @property(nonatomic, assign, readonly) BOOL neonAppearance;
 @property(nonatomic, assign, readonly) BOOL mechanicalAppearance;
+@property(nonatomic, assign, readonly) ZNThemeDecorationStyle decorationStyle;
 
 @property(nonatomic, strong, readonly) UIColor *panelColor;
 @property(nonatomic, strong, readonly) UIColor *headerColor;
@@ -42,5 +80,4 @@ typedef NS_ENUM(NSInteger, ZNThemeMode) {
 + (NSString *)nameForMode:(ZNThemeMode)mode;
 + (UIColor *)indicatorColorForMode:(ZNThemeMode)mode;
 + (ZNTheme *)themeForMode:(ZNThemeMode)mode interfaceStyle:(UIUserInterfaceStyle)style;
-
 @end
