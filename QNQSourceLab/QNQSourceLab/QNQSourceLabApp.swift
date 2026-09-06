2631fc82838a9170412919491e273a268a2404b5
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-06 23:02:29 +0800

    Bootstrap bundled V2 key on app launch

--- QNQSourceLab/QNQSourceLab/QNQSourceLabApp.swift
+++ QNQSourceLab/QNQSourceLab/QNQSourceLabApp.swift
@@ -2,9 +2,25 @@ import SwiftUI
 
 @main
 struct QNQSourceLabApp: App {
+    init() {
+        Self.installBundledV2KeyIfPresent()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+
+    private static func installBundledV2KeyIfPresent() {
+        guard let sourceURL = Bundle.main.url(forResource: "appstore_v2", withExtension: "pem") else {
+            return
+        }
+        guard let data = try? Data(contentsOf: sourceURL), !data.isEmpty else {
+            return
+        }
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let targetURL = documentDirectory.appendingPathComponent("appstore_v2.pem")
+        try? data.write(to: targetURL, options: .atomic)
     }
 }
