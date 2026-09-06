45ace7c813fb597a811e01189b2eee1ff05def07
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-06 23:45:26 +0800

    runtime menu: add marker-only developer identity source

--- iosruntimepatchmenu/src/ZNDeveloperGate.h
+++ iosruntimepatchmenu/src/ZNDeveloperGate.h
@@ -7,6 +7,7 @@ typedef NS_ENUM(NSInteger, ZNIdentitySource) {
     ZNIdentitySourceHostDylib,
     ZNIdentitySourceZonoeLocalTicket,
     ZNIdentitySourceSubmittedHost,
+    ZNIdentitySourceMarkerFile,
 };
 
 @interface ZNDeveloperGate : NSObject
