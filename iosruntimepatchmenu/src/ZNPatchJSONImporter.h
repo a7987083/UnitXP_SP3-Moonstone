e621db46b9298f82da34001370b1cc84e97e99f8
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-08 17:41:40 +0800

    fix(json): expose marker-sibling discovery diagnostics

--- iosruntimepatchmenu/src/ZNPatchJSONImporter.h
+++ iosruntimepatchmenu/src/ZNPatchJSONImporter.h
@@ -3,6 +3,7 @@
 NS_ASSUME_NONNULL_BEGIN
 @interface ZNPatchJSONImporter : NSObject
 + (NSArray<NSString *> *)discoverJSONFiles;
++ (NSString *)discoveryStatus;
 + (nullable NSArray<NSDictionary *> *)importFile:(NSString *)path error:(NSString * _Nullable * _Nullable)error;
 @end
 NS_ASSUME_NONNULL_END
