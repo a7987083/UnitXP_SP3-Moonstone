3aa0d1a3919392b2cac07b403fe5fff467f0ae22
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-12 12:28:19 +0800

    feat(runtime-patch): add generated Mach-O signing API

---
+++ iosruntimepatchmenu/src/ZNAdhocMachOSigner.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+BOOL ZNAdhocResignMachOAtPath(NSString *path,
+                              NSDictionary * _Nullable * _Nullable metadata,
+                              NSString * _Nullable * _Nullable error);
+NS_ASSUME_NONNULL_END
