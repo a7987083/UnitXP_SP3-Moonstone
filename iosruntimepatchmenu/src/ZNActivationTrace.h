0432c3f6405c490c500cf5204804bbd6953b5d31
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-14 03:08:47 +0800

    feat(v0.5.6.1): add activation trace interface

---
+++ iosruntimepatchmenu/src/ZNActivationTrace.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT void ZNActivationTraceLog(NSString *message);
+FOUNDATION_EXPORT NSString *ZNActivationTraceLogPath(void);
+FOUNDATION_EXPORT double ZNActivationTraceNow(void);
+
+NS_ASSUME_NONNULL_END
