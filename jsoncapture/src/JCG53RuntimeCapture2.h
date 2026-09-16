79472eaa6cd8d024867ccdc784b2f1921dce46c8
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-17 07:48:43 +0800

    feat(v0.5.3): add runtime capture 2 interface

---
+++ jsoncapture/src/JCG53RuntimeCapture2.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT void JCG53Runtime2Start(void);
+FOUNDATION_EXPORT void JCG53Runtime2SetEnabled(BOOL enabled);
+FOUNDATION_EXPORT BOOL JCG53Runtime2IsEnabled(void);
+FOUNDATION_EXPORT NSString *JCG53Runtime2StatusText(void);
