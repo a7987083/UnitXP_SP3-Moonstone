4d800770a3cc2c9da19310bfe6e6ecec261004c1
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-23 07:20:21 +0800

    feat(m4.6): add full method signature API

---
+++ iosruntimepatchmenu/src/ZNIL2CPPMethodSignature.h
@@ -0,0 +1,27 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT NSString *ZNIL2CPPEncodeParameterTypeNames(NSArray<NSString *> *types);
+FOUNDATION_EXPORT NSArray<NSString *> *ZNIL2CPPDecodeParameterTypeNames(NSString *encoded);
+FOUNDATION_EXPORT nullable NSArray<NSString *> *ZNIL2CPPParameterTypeNamesForCandidate(NSDictionary<NSString *, id> *candidate,
+                                                                                       NSString * _Nullable * _Nullable error);
+FOUNDATION_EXPORT NSString *ZNIL2CPPFullMethodIdentity(NSString *assembly,
+                                                       NSString *namespaceName,
+                                                       NSString *className,
+                                                       NSString *methodName,
+                                                       NSArray<NSString *> *parameterTypeNames);
+FOUNDATION_EXPORT NSString *ZNIL2CPPShortSignature(NSString *methodName,
+                                                   NSArray<NSString *> *parameterTypeNames);
+
+@interface ZNIL2CPPFullSignatureResolver : NSObject
++ (instancetype)sharedResolver;
+- (nullable NSDictionary<NSString *, id> *)resolveAssembly:(NSString *)assembly
+                                                 namespace:(NSString *)namespaceName
+                                                 className:(NSString *)className
+                                                    method:(NSString *)methodName
+                                        parameterTypeNames:(NSArray<NSString *> *)parameterTypeNames
+                                                     error:(NSString * _Nullable * _Nullable)error;
+@end
+
+NS_ASSUME_NONNULL_END
