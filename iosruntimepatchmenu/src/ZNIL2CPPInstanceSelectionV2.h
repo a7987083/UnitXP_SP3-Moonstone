d21f13c6a5be5f080afa22a8a01396ee80f3ddc4
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-23 00:01:57 +0800

    feat: add M4.4 instance selection API

---
+++ iosruntimepatchmenu/src/ZNIL2CPPInstanceSelectionV2.h
@@ -0,0 +1,26 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZNIL2CPPInstanceResolver (ZNInstanceSelectionV2)
+- (uintptr_t)znm44_selectedInstanceForAssembly:(NSString *)assembly
+                                     namespace:(NSString *)namespaceName
+                                     className:(NSString *)className;
+- (BOOL)znm44_selectInstanceAddress:(uintptr_t)address
+                           assembly:(NSString *)assembly
+                          namespace:(NSString *)namespaceName
+                          className:(NSString *)className
+                              error:(NSString * _Nullable * _Nullable)error;
+- (void)znm44_clearSelectedInstanceForAssembly:(NSString *)assembly
+                                      namespace:(NSString *)namespaceName
+                                      className:(NSString *)className;
+- (BOOL)znm44_validateInstanceAddress:(uintptr_t)address
+                             assembly:(NSString *)assembly
+                            namespace:(NSString *)namespaceName
+                            className:(NSString *)className
+                                error:(NSString * _Nullable * _Nullable)error;
+@end
+
+extern "C" void ZNInstallIL2CPPInstanceSelectionV2Deferred(void);
+
+NS_ASSUME_NONNULL_END
